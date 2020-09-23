68e6e61f554027cb65571290ec65f0777c3c7bdd
DatLe <ledatdx97@gmail.com>
2020-09-23 14:06:31 +0700

    fix error css login form

--- resources/views/login/loginform.blade.php
+++ resources/views/login/loginform.blade.php
@@ -17,7 +17,7 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="{{asset('css/adminlte.min.css')}}">
   {{-- main css --}}
-  <link rel="stylesheet" href="{{asset('css/main.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/css/login.css')}}">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
